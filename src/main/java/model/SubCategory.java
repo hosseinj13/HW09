6171bc70722b22f7cf91e5233a05590be8a0acce
hossein javadi <hosseinj13@yahoo.com>
2024-02-14 00:17:27 +0330

    create SubCategory.java and add (AllArgsConstructor, NoArgsConstructor, Getter, Setter, ToString) with lombok.

---
+++ src/main/java/model/SubCategory.java
@@ -0,0 +1,28 @@
+package model;
+
+import base.model.BaseEntity;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@ToString
+
+
+public class SubCategory extends BaseEntity<Integer> {
+    String subCategoryName;
+    Category category;
+
+    public SubCategory(Integer subCategoryId, String subCategoryName, Category category ){
+        super(subCategoryId);
+        this.subCategoryName = subCategoryName;
+        this.category = category;
+    }
+
+    public SubCategory(String subCategoryName) {
+        this.subCategoryName = subCategoryName;
+    }
+}

6059113c2b0b10c02955d5669fc346769600a7fa
hossein javadi <hosseinj13@yahoo.com>
2024-02-13 23:51:52 +0330

    create BaseEntity.java and add (AllArgsConstructor, NoArgsConstructor, Getter, Setter, ToString) with lombok.

---
+++ src/main/java/base/model/BaseEntity.java
@@ -0,0 +1,17 @@
+package base.model;
+
+import lombok.*;
+import java.io.Serializable;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+
+public class BaseEntity <ID extends Serializable> {
+    private ID id;
+}
+
+
